93263acc16120b5527f7f89ae0166ce9a3410d07
Katka Martínková <martinkova.katie@gmail.com>
2021-05-01 14:15:38 +0200

    hiding navigation menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtnElm = document.querySelector('#nav-btn');
+const navMenuItemElms = document.querySelectorAll('.navigation nav a');
+
+const closeMenu = () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+};
+
+navBtnElm.addEventListener('click', closeMenu);
+navMenuItemElms.forEach((element) => {
+  element.addEventListener('click', closeMenu);
+});
