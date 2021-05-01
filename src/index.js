8c1ee861f17fecda337b89b9f6053e1573973849
Katka Martínková <martinkova.katie@gmail.com>
2021-05-01 14:32:51 +0200

    order button logic

--- src/index.js
+++ src/index.js
@@ -12,3 +12,19 @@ navBtnElm.addEventListener('click', closeMenu);
 navMenuItemElms.forEach((element) => {
   element.addEventListener('click', closeMenu);
 });
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let isOrdered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (isOrdered === true) {
+    orderBtnElm.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    isOrdered = !isOrdered;
+  } else {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    isOrdered = !isOrdered;
+  }
+});
